04451e4b71a1a84ed88c3e3bac2f29f0464b7127
lizkavalski <lizkavalski@DESKTOP-IM8BUGM.localdomain>
2019-08-06 05:35:55 -0700

    still needs tests

---
+++ trees/README.md
@@ -0,0 +1,13 @@
+# Trees
+A Data structure that makes it easier to look through data
+## Challenge
+Create a class for Binary Tree that has methods called preOrder, inOrder and postOrder that orgainze the data in a curtain order.
+
+Along with creating a class for Binary Search Tree that has an add methods that add a value and a new node to the the tree. Then also a contains methods that return either ture or false if the value is in the tree
+## Approach & Efficiency
+The Binary Tree is what was done in class on Saturday. The Binary Search Tree for add I looked if it has a root, if not give it one. Then set the root as current. Then looked at the current value to determine if it gose left or right.
+
+The contain part in the Binary Search tree class I check it their is a root or if the value is the same,which return either true, or faluse. Then it look through each of the branchs it see if their any matchs
+
+## API
+At this point no test has been made.S
---
+++ trees/tree.js
@@ -0,0 +1,97 @@
+import { newExpression } from "@babel/types";
+
+class Node{
+    constructor(){
+    this.value =value;
+    this.left = null;
+    this.right =null;
+    }
+  }
+  
+  class BinaryTree{
+    constructor(root = null){
+      this.root = root
+  
+    };
+    preOrder(){
+      let results =[];
+       let _walk= node =>{
+         results.push(node.value);
+         if(node.left){_walk(node.left)}
+         if(node.right){_walk(node.right)}
+       };
+       _walk(this.root);
+  
+       return results;
+    };
+    inOrder(){ 
+      let results =[];
+        let _walk= node =>{
+          if(node.left){_walk(node.left)}
+          results.push(node.value);
+          if(node.right){_walk(node.right)}
+        };
+        _walk(this.root);
+  
+        return results;
+    }
+    postOrder(){
+      let results =[];
+       let _walk= node =>{
+         if(node.left){_walk(node.left)}
+         if(node.right){_walk(node.right)}
+        results.push(node.value);
+       };
+       _walk(this.root);
+  
+       return results;
+    }
+    levelOrder(){
+      let results =[];
+      let nodeQueue=[];
+  
+      nodeQueue.push(this.root);
+  
+      while(nodeQueue.length){
+        let current = nodeQueue.shift();
+        results.push(current.value);
+        if(current.left){nodeQueue.push(current.left)};
+        if(current.right){nodeQueue.push(current.right)};
+      }
+    }
+
+    }
+    class BinarySearchTree{
+        constructor(root = null){
+          this.root = root
+      
+        };
+    }
+    add(value){
+        if(this.root = null){
+            this.root = new Node(value);
+        }
+        let current = this.root
+        while(current){
+            if(current.value < value ){
+                this.current.right = new Node(value);
+            }
+            if(current.value > value){
+                this.current.left = new Node(value);
+            }
+        }
+    };
+    contains(value){
+        if(this.root === null){
+            return false
+        }
+        if(this.root === value){
+            return true;
+        }
+        else if(root.value > value){
+            return this.contains(value,current.left);
+        } else{
+            return this.contains(value, current.right)
+        }
+
+    };
